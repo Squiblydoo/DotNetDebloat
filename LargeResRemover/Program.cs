0825c7c9a2eea300c12cd2ff8b44355795fa2f6c
Squiblydoo <squiblydoo@pm.me>
2024-05-04 06:17:01 -0400

    Initial release
    Initial release

---
+++ LargeResRemover/Program.cs
@@ -0,0 +1,58 @@
+﻿using AsmResolver;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Serialized;
+using AsmResolver.DotNet.Builder;
+
+namespace LargeResRemover;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+		if (args.Length == 0)
+		{
+			Console.WriteLine("No input file given.");
+			return;
+		}
+
+		// Read file.		
+		string inputPath = args[0];
+		var module = ModuleDefinition.FromFile(
+			inputPath,
+			new ModuleReaderParameters(EmptyErrorListener.Instance) // optional: but can be good for obfuscated binaries to ignore reader errors.
+		);
+
+		// Strip large resources.
+		StripLargeResources(module);
+
+		// Save modified file to file.patched.extension.
+		string outputPath = Path.ChangeExtension(inputPath, ".patched" + Path.GetExtension(inputPath));
+		module.Write(
+			outputPath, 
+			new ManagedPEImageBuilder(EmptyErrorListener.Instance) // optional: but can be good for obfuscated binaries to ignore writer errors.
+		);
+	}
+
+	public static void StripLargeResources(ModuleDefinition module)
+	{
+		const uint SizeThreshold = 1024*1024; // 1MB
+
+		// Loop over all managed resources.
+		for (int i = 0; i < module.Resources.Count; i++)
+		{
+			var resource = module.Resources[i];
+
+			// Measure size of resource data.
+			uint resourceSize = resource.EmbeddedDataSegment?.GetPhysicalSize() ?? 0;
+
+			// Remove when too large.
+			if (resourceSize > SizeThreshold)
+			{
+				Console.WriteLine($"Removing large resource {resource.Name} ({resourceSize} bytes)");
+				module.Resources.RemoveAt(i);
+				i--;
+			}
+		}
+	}
+}
+
---
+++ SingleFileSolution/Program.cs
@@ -0,0 +1,50 @@
+﻿using AsmResolver;
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Serialized;
+using AsmResolver.DotNet.Builder;
+using AsmResolver.DotNet.Bundles;
+using System.Text;
+
+namespace SingleFileSolution;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+
+        if (args.Length == 0)
+		{
+			Console.WriteLine("No input file given.");
+			return;
+		}
+
+		// Read file.		
+		string inputPath = args[0];
+
+		var bundle = new BundleFile(inputPath);
+		var manifest = BundleManifest.FromFile( inputPath );
+		var bundle_relativePath = manifest.Files[0].RelativePath;
+
+
+		foreach (var file in manifest.Files)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(inputPath);
+            string subdirectory = fileName;
+            string path = Path.Combine(subdirectory, file.RelativePath);
+            byte[] contents = file.GetData();
+
+            // Ensure the directory exists
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+            Console.WriteLine($"Extracting {path} {contents.Length} bytes");
+            System.IO.File.WriteAllBytes(path, contents);
+        
+		}
+
+
+        Console.ReadKey();
+
+    }
+
+}
+
